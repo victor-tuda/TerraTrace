aaf2672517c50fe45e5ed11e87b2c973fed9d2e9
Victor Tuda <victor_gtuda@outlook.com>
2023-08-22 16:12:09 -0300

    feat: adicionada api modelo

---
+++ TerraTraceApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "terratraceapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
